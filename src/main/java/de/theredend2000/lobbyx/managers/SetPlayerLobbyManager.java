7dd55e945c405946b19dd8c843a44e52c338f87f
TheRedEnd2000 <fkircher2007@gmail.com>
2023-02-22 21:06:00 +0100

    NEW

--- src/main/java/de/theredend2000/lobbyx/managers/SetPlayerLobbyManager.java
+++ src/main/java/de/theredend2000/lobbyx/managers/SetPlayerLobbyManager.java
@@ -2,7 +2,9 @@ package de.theredend2000.lobbyx.managers;
 
 import de.theredend2000.lobbyx.Main;
 import de.theredend2000.lobbyx.messages.Util;
+import de.theredend2000.lobbyx.util.ConfigLocationUtil;
 import de.theredend2000.lobbyx.util.ItemBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -33,7 +35,21 @@ public class SetPlayerLobbyManager {
             player.setFoodLevel(20);
             //HEAL
             setItems(player);
-            plugin.getTablistManager().setPlayerList(player);
+
+            if(plugin.getConfig().getBoolean("Titles.WelcomeTitle.enabled"))
+                player.sendTitle(plugin.getConfig().getString("Titles.WelcomeTitle.title").replaceAll("&","§").replaceAll("%PLAYER%",player.getName()),plugin.getConfig().getString("Titles.WelcomeTitle.subtitle").replaceAll("&","§").replaceAll("%PLAYER%",player.getName()),20,100,40);
+
+            ConfigLocationUtil locationUtil = new ConfigLocationUtil(plugin,"Locations.Lobby");
+            if(locationUtil.loadLocation() != null){
+                player.teleport(locationUtil.loadLocation());
+            }else{
+                for(Player admins : Bukkit.getOnlinePlayers()){
+                    if(admins.isOp()){
+                        admins.sendMessage("§cThe Lobby is not set. Please do this now!");
+                    }
+                }
+            }
+
         }
     }
 
@@ -50,11 +66,11 @@ public class SetPlayerLobbyManager {
         if(plugin.getConfig().getBoolean("Items.Profile.enabled"))
             player.getInventory().setItem(plugin.getConfig().getInt("Items.Profile.slot"),playerHead);
         if(plugin.yaml.getBoolean("Settings."+player.getUniqueId()+".PlayerHidden")){
-            if(plugin.getConfig().getBoolean("Items.Hider_ON.enabled"))
-                player.getInventory().setItem(plugin.getConfig().getInt("Items.Hider_ON.slot"),new ItemBuilder(plugin.getMaterial("Items.Hider_ON.material")).setDisplayname(plugin.getConfig().getString("Items.Hider_ON.displayname").replaceAll("&","§")).setLocalizedName("lobbyx.player_hider").build());
-        }else {
             if(plugin.getConfig().getBoolean("Items.Hider_OFF.enabled"))
                 player.getInventory().setItem(plugin.getConfig().getInt("Items.Hider_OFF.slot"),new ItemBuilder(plugin.getMaterial("Items.Hider_OFF.material")).setDisplayname(plugin.getConfig().getString("Items.Hider_OFF.displayname").replaceAll("&","§")).setLocalizedName("lobbyx.player_hider").build());
+        }else {
+            if(plugin.getConfig().getBoolean("Items.Hider_ON.enabled"))
+                player.getInventory().setItem(plugin.getConfig().getInt("Items.Hider_ON.slot"),new ItemBuilder(plugin.getMaterial("Items.Hider_ON.material")).setDisplayname(plugin.getConfig().getString("Items.Hider_ON.displayname").replaceAll("&","§")).setLocalizedName("lobbyx.player_hider").build());
         }
         if(plugin.getConfig().getBoolean("Items.Teleporter.enabled"))
             player.getInventory().setItem(plugin.getConfig().getInt("Items.Teleporter.slot"),new ItemBuilder(plugin.getMaterial("Items.Teleporter.material")).setDisplayname(plugin.getConfig().getString("Items.Teleporter.displayname").replaceAll("&","§")).build());
--- src/main/java/de/theredend2000/lobbyx/managers/TablistManager.java
+++ src/main/java/de/theredend2000/lobbyx/managers/TablistManager.java
@@ -1,7 +1,9 @@
 package de.theredend2000.lobbyx.managers;
 
 import de.theredend2000.lobbyx.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ListIterator;
 
@@ -11,6 +13,8 @@ public class TablistManager {
 
     public TablistManager(Main plugin){
         this.plugin = plugin;
+
+        updateTabList();
     }
 
     public void setPlayerList(Player player){
@@ -19,12 +23,27 @@ public class TablistManager {
                 .replaceAll("%SERVER_ONLINE%",String.valueOf(player.getServer().getOnlinePlayers().size()))
                 .replaceAll("%SERVER_MAX_PLAYERS%",String.valueOf(player.getServer().getMaxPlayers()))
                 .replaceAll("%PLAYER_SERVER%",player.getServer().getName())
+                .replaceAll("%PLAYER_PING%", String.valueOf(player.getPing()))
                 .replaceAll("~","\n")
                 ,plugin.getConfig().getString("Tablist.Footer").replaceAll("&","§")
                         .replaceAll("%SERVER_ONLINE%",String.valueOf(player.getServer().getOnlinePlayers().size()))
                         .replaceAll("%SERVER_MAX_PLAYERS%",String.valueOf(player.getServer().getMaxPlayers()))
                         .replaceAll("%PLAYER_SERVER%",player.getServer().getName())
+                        .replaceAll("%PLAYER_PING%", String.valueOf(player.getPing()))
                         .replaceAll("~","\n"));
+    }
+
+    private void updateTabList(){
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for(Player player : Bukkit.getServer().getOnlinePlayers()){
+                    if(plugin.getLobbyWorlds().contains(player.getWorld())){
+                        setPlayerList(player);
+                    }
+                }
+            }
+        }.runTaskTimer(plugin,0,plugin.getConfig().getInt("Tablist.Update_Delay"));
     }
 
 }
