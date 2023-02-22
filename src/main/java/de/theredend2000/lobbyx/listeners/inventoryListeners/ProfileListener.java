4f363e0bb76d63d40865682e96b04ae6db6017c3
Cet1n <mavoelter725@gmail.com>
2023-02-22 22:43:08 +0100

    i

--- src/main/java/de/theredend2000/lobbyx/listeners/inventoryListeners/ProfileListener.java
+++ src/main/java/de/theredend2000/lobbyx/listeners/inventoryListeners/ProfileListener.java
@@ -2,9 +2,12 @@ package de.theredend2000.lobbyx.listeners.inventoryListeners;
 
 import de.theredend2000.lobbyx.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+
+import java.util.Objects;
 
 public class ProfileListener implements Listener {
 
@@ -17,6 +20,25 @@ public class ProfileListener implements Listener {
 
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event){
+        Player player = (Player) event.getWhoClicked();
+        if (event.getView().getTitle().equals(Objects.requireNonNull(plugin.getConfig().getString("Inventory.ProfileInventoryTitle")).replaceAll("&","§"))){event.setCancelled(true);if (event.getCurrentItem()!=null){if (event.getCurrentItem().getItemMeta().hasLocalizedName()){
+                switch(event.getCurrentItem().getItemMeta().getLocalizedName()){
+                    case "MainInventory.Profil":
+                        plugin.getProfileMenuManager();
+                        break;
+                    case "MainInventory.Stats":
+                        break;
+                    case"MainInventory.Settings":
+                        break;
+                    case"MainInventory.Sozial":
+                        break;
+
+                }
+            }
+        }
+        }
+
+
 
     }
 
--- src/main/java/de/theredend2000/lobbyx/managers/ProfileMenuManager.java
+++ src/main/java/de/theredend2000/lobbyx/managers/ProfileMenuManager.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.material.Skull;
 
 import java.util.Objects;
 
@@ -23,8 +24,8 @@ public class ProfileMenuManager implements Listener {
 
     public void createProfileInventory(Player player){
         Inventory inventory = Bukkit.createInventory(player, 54, Objects.requireNonNull(plugin.getConfig().getString("Inventory.ProfileInventoryTitle")).replaceAll("&","§"));
-        int[] blueglass = new int[]{1,2,3,4,5,6,7,9,11,12,13,14,14,17,18,21,23,26,27,20,31,32,35,36,39,40,41,44,46,47,48,49,50,51,52};
-        int [] anderesglass = new int[]{0,8,10,16,19,20,24,25,28,34,37,38,42,43,45};
+        int[] blueglass = new int[]{1,2,3,4,5,6,7,9,11,12,13,14,15,17,18,21,23,26,27,20,31,32,35,36,39,40,41,44,46,47,48,49,50,51,52};
+        int [] anderesglass = new int[]{0,8,10,16,19,20,24,25,28,34,37,38,42,43,45,53};
         for (int i = 0; i<blueglass.length;i++){inventory.setItem(blueglass[i], new ItemBuilder(Material.BLUE_STAINED_GLASS_PANE).setDisplayname("§c").build());}
         for (int i = 0; i<anderesglass.length;i++){inventory.setItem(anderesglass[i], new ItemBuilder(Material.CYAN_STAINED_GLASS_PANE).setDisplayname("§c").build());}
         ItemStack playerhead = new ItemStack(Material.PLAYER_HEAD);SkullMeta skullMeta = (SkullMeta) playerhead.getItemMeta();skullMeta.setOwner(player.getName());skullMeta.setDisplayName("Profile");skullMeta.setLocalizedName("MainInventory.Profil");playerhead.setItemMeta(skullMeta);
@@ -34,6 +35,16 @@ public class ProfileMenuManager implements Listener {
         inventory.setItem(40, new ItemBuilder(Material.DIAMOND).setDisplayname("Sozial").setLocalizedName("MainInventory.Sozial").build());
         player.openInventory(inventory);
 
+    }
+    public  void createSozialInventory(Player player){
+        Inventory s = Bukkit.createInventory(player,54,Objects.requireNonNull(plugin.getConfig().getString("Inventory.SozailInventoryTitle")).replaceAll("&","§"));
+        int[] blueglass = new int[]{0,1,2,3,5,6,7,8,9,10,11,15,16,17,18,26};
+        int[] whiteglass = new int[]{12,13,14,39,40,41};
+        int[] anderesblauglass = new int[]{27,35,36,37,38,42,43,44,46,47,48,50,51,52,53};
+        for (int i = 0; i<blueglass.length;i++){s.setItem(blueglass[i], new ItemBuilder(Material.BLUE_STAINED_GLASS_PANE).setDisplayname("§c").build());}
+        for (int i = 0; i<whiteglass.length;i++){s.setItem(whiteglass[i], new ItemBuilder(Material.BLUE_STAINED_GLASS_PANE).setDisplayname("§c").build());}
+        for (int i = 0; i<anderesblauglass.length;i++){s.setItem(anderesblauglass[i], new ItemBuilder(Material.BLUE_STAINED_GLASS_PANE).setDisplayname("§c").build());}
+        ItemStack playerhead = new ItemStack(Material.PLAYER_HEAD);SkullMeta skullMeta = (SkullMeta) playerhead.getItemMeta();
     }
 
 }
