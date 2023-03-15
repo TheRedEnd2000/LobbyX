4c52be04787bcc4507f23835871a2e4242ea5832
Cet1n <mavoelter725@gmail.com>
2023-03-15 15:56:44 +0100

    1.12.4

--- src/main/java/de/theredend2000/lobbyx/listeners/PlayerInteractAtEntityEventListener.java
+++ src/main/java/de/theredend2000/lobbyx/listeners/PlayerInteractAtEntityEventListener.java
@@ -45,9 +45,6 @@ public class PlayerInteractAtEntityEventListener implements Listener {
         Inventory Playerinfo = Bukkit.createInventory(player, 54,Objects.requireNonNull(plugin.getConfig().getString("Inventory.InteractwithplayerInventory")).replaceAll("&", "§"));
         int[] white = new int[]{10,12,14,16,19,21,22,23,24,25,28,30,31,32,33,34,37,39,41,43};
         int[] orange = new int[]{0,1,2,3,5,6,7,8,9,17,18,26,27,35,36,44,45,46,47,48,50,51,52};
-        String Language = (String);
-
-
         Playerinfo.setItem(4,new ItemBuilder(Material.RED_STAINED_GLASS_PANE).setDisplayname("§c").build());
         Playerinfo.setItem(49,new ItemBuilder(Material.RED_STAINED_GLASS_PANE).setDisplayname("§c").build());
         for (int i = 0; i < white.length; i++) {Playerinfo.setItem(white[i], new ItemBuilder(Material.WHITE_STAINED_GLASS_PANE).setDisplayname("§c").build());}
