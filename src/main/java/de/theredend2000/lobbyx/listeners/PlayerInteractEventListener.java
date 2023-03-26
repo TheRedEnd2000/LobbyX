4d9b9b2b9abfc441cac1a4531d4d1d3aaae4713d
Cet1n <mavoelter725@gmail.com>
2023-03-26 01:59:47 +0100

    1.14.3

--- src/main/java/de/theredend2000/lobbyx/listeners/PlayerInteractEventListener.java
+++ src/main/java/de/theredend2000/lobbyx/listeners/PlayerInteractEventListener.java
@@ -51,8 +51,18 @@ public class PlayerInteractEventListener implements Listener {
                         }
                     }
                     if (plugin.getConfig().getBoolean("IntractableItems.Doors")) {
-                        if (event.getClickedBlock().getType() == Material.OAK_DOOR | event.getClickedBlock().getType() == Material.IRON_DOOR | event.getClickedBlock().getType() == Material.WARPED_DOOR | event.getClickedBlock().getType() == Material.SPRUCE_DOOR | event.getClickedBlock().getType() == Material.BIRCH_DOOR | event.getClickedBlock().getType() == Material.JUNGLE_DOOR | event.getClickedBlock().getType() == Material.ACACIA_DOOR | event.getClickedBlock().getType() == Material.DARK_OAK_DOOR | event.getClickedBlock().getType() == Material.MANGROVE_DOOR | event.getClickedBlock().getType() == Material.CRIMSON_DOOR) {
+                        if (event.getClickedBlock().getType() == Material.OAK_DOOR | event.getClickedBlock().getType() == Material.IRON_DOOR  | event.getClickedBlock().getType() == Material.SPRUCE_DOOR | event.getClickedBlock().getType() == Material.BIRCH_DOOR | event.getClickedBlock().getType() == Material.JUNGLE_DOOR | event.getClickedBlock().getType() == Material.ACACIA_DOOR | event.getClickedBlock().getType() == Material.DARK_OAK_DOOR) {
                             event.setCancelled(true);
+                            if (plugin.getServer().getVersion().contains("1.16")){
+                                if (event.getClickedBlock().getType() == Material.CRIMSON_DOOR | event.getClickedBlock().getType() == Material.WARPED_DOOR){
+                                    event.setCancelled(true);
+                                    if (plugin.getServer().getVersion().contains("1.19")){
+                                        if (event.getClickedBlock().getType() == Material.MANGROVE_DOOR){
+                                            event.setCancelled(true);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                     if (plugin.getConfig().getBoolean("IntractableItems.Buttons")) {
@@ -71,8 +81,13 @@ public class PlayerInteractEventListener implements Listener {
                         }
                     }
                     if (plugin.getConfig().getBoolean("IntractableItems.Boats")) {
-                        if (event.getClickedBlock().getType() == Material.OAK_BOAT | event.getClickedBlock().getType() == Material.OAK_CHEST_BOAT | event.getClickedBlock().getType() == Material.SPRUCE_BOAT | event.getClickedBlock().getType() == Material.CARTOGRAPHY_TABLE | event.getClickedBlock().getType() == Material.GRINDSTONE | event.getClickedBlock().getType() == Material.SMOKER | event.getClickedBlock().getType() == Material.BARREL | event.getClickedBlock().getType() == Material.SMITHING_TABLE | event.getClickedBlock().getType() == Material.LOOM) {
+                        if (event.getClickedBlock().getType() == Material.OAK_BOAT | event.getClickedBlock().getType() == Material.SPRUCE_BOAT | event.getClickedBlock().getType() == Material.BIRCH_BOAT | event.getClickedBlock().getType() == Material.JUNGLE_BOAT | event.getClickedBlock().getType() == Material.ACACIA_BOAT | event.getClickedBlock().getType() == Material.DARK_OAK_BOAT) {
                             event.setCancelled(true);
+                            if (plugin.getServer().getVersion().contains("1.19")){
+                                if (event.getClickedBlock().getType() == Material.MANGROVE_BOAT){
+                                    event.setCancelled(true);
+                                }
+                            }
                         }
 
                         if(plugin.getServer().getVersion().contains("1.19")){
@@ -102,10 +117,11 @@ public class PlayerInteractEventListener implements Listener {
                             }if (plugin.getServer().getVersion().contains("1.16")){
                                 if (event.getClickedBlock().getType() == Material.CRIMSON_TRAPDOOR | event.getClickedBlock().getType() == Material.WARPED_TRAPDOOR){
                                     event.setCancelled(true);
-                                }
-                            }if (plugin.getServer().getVersion().contains("1.19")){
-                                if (event.getClickedBlock().getType() == Material.MANGROVE_TRAPDOOR){
-                                    event.setCancelled(true);
+                                    if (plugin.getServer().getVersion().contains("1.19")) {
+                                        if (event.getClickedBlock().getType() == Material.MANGROVE_TRAPDOOR) {
+                                            event.setCancelled(true);
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -113,10 +129,26 @@ public class PlayerInteractEventListener implements Listener {
                     }
                     if (plugin.getConfig().getBoolean("IntractableItems.Candles")){
                         if (plugin.getServer().getVersion().contains("1.17")){
-                            event.setCancelled(true);
+                            if (event.getClickedBlock().getType() == Material.CANDLE | event.getClickedBlock().getType() == Material.WHITE_CANDLE | event.getClickedBlock().getType() == Material.ORANGE_CANDLE | event.getClickedBlock().getType() == Material.MAGENTA_CANDLE | event.getClickedBlock().getType() == Material.LIGHT_BLUE_CANDLE | event.getClickedBlock().getType() == Material.YELLOW_CANDLE | event.getClickedBlock().getType() == Material.LIME_CANDLE | event.getClickedBlock().getType() == Material.PINK_CANDLE | event.getClickedBlock().getType() == Material.GRAY_CANDLE | event.getClickedBlock().getType() == Material.LIGHT_GRAY_CANDLE | event.getClickedBlock().getType() == Material.CYAN_CANDLE | event.getClickedBlock().getType() == Material.PURPLE_CANDLE | event.getClickedBlock().getType() == Material.BLUE_CANDLE | event.getClickedBlock().getType() == Material.BROWN_CANDLE | event.getClickedBlock().getType() == Material.GREEN_CANDLE | event.getClickedBlock().getType() == Material.RED_CANDLE | event.getClickedBlock().getType() == Material.BLACK_CANDLE){
+                                event.setCancelled(true);
+                            }
                         }
                     }
-                    if (plugin.getConfig().getBoolean("IntractableItems")){
+                    if (plugin.getConfig().getBoolean("IntractableItems.Other")){
+                        if (event.getClickedBlock().getType() == Material.BEACON | event.getClickedBlock().getType() == Material.ENCHANTING_TABLE | event.getClickedBlock().getType() == Material.NOTE_BLOCK | event.getClickedBlock().getType() == Material.JUKEBOX | event.getClickedBlock().getType() == Material.BREWING_STAND | event.getClickedBlock().getType() == Material.CAULDRON | event.getClickedBlock().getType() == Material.FLOWER_POT){
+                            event.setCancelled(true);
+                            if (plugin.getServer().getVersion().contains("1.13")){
+                                if (event.getClickedBlock().getType() == Material.BEE_NEST | event.getClickedBlock().getType() == Material.BEEHIVE){
+                                    event.setCancelled(true);
+                                    if (plugin.getServer().getVersion().contains("1.16")){
+                                        if (event.getClickedBlock().getType() == Material.RESPAWN_ANCHOR){
+                                            event.setCancelled(true);
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
 
                     }
                     if (plugin.getConfig().getBoolean("IntractableItems")){
